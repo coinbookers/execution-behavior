f0d20fbab92c5919c773037d93aa4f2f8aec24c0
Coinbookers <91235209+coinbookers@users.noreply.github.com>
2026-06-28 22:14:45 +0100

    Create execution-behavior.py output
    output

---
+++ execution-behavior.py
@@ -0,0 +1,62 @@
+import json
+
+from web3 import Web3
+from eth_account import Account
+
+NODE = "https://rpc.example.org"
+PRIVATE = "YOUR_PRIVATE_KEY"
+
+terms = (
+    "illustrates",
+    "endpoint",
+    "delivers",
+)
+
+rpc = Web3(
+    Web3.HTTPProvider(NODE)
+)
+
+wallet = Account.from_key(
+    PRIVATE
+)
+
+events = []
+
+for item in terms:
+    events.append(
+        {
+            "value": item
+        }
+    )
+
+transaction = {
+    "from": wallet.address,
+    "to": "0x0000000000000000000000000000000000000000",
+    "value": 0,
+    "gas": 120000,
+    "gasPrice": rpc.to_wei(
+        4,
+        "gwei"
+    ),
+    "nonce": rpc.eth.get_transaction_count(
+        wallet.address
+    ),
+    "chainId": 1,
+}
+
+signed = wallet.sign_transaction(
+    transaction
+)
+
+output = {
+    "wallet": wallet.address,
+    "size": len(
+        signed.raw_transaction.hex()
+    ),
+    "events": events,
+}
+
+with open(
+    "endpoint.json",
+    "w"
+) as fi
